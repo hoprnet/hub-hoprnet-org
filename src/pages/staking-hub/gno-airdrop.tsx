9cb33f9b68c21736bf8d7687d006d8bbe3091ba6
Michał Jadach <michal.jadach@hoprnet.org>
2024-07-25 16:06:07 +0200

    <strong>August 8th</strong>

--- src/pages/staking-hub/gno-airdrop.tsx
+++ src/pages/staking-hub/gno-airdrop.tsx
@@ -296,7 +296,7 @@ function WrapperPage() {
                 tutorial: how to generate deposits files
               </a>
               ).<br /><br />
-              You have until<strong>August 8th</strong>to submit your validator(s) file. If you miss this deadline, you will have to wait until future distributions, which are first come, first served.
+              You have until<strong>{` `}August 8th{` `}</strong>to submit your validator(s) file. If you miss this deadline, you will have to wait until future distributions, which are first come, first served.
               <br /><br />
             </span>
           </>
